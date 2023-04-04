8d47a23509be5e4a0178003dcd6a1e33cd64b206
Vladislav Sorokin <sorokinvj@gmail.com>
2023-04-05 00:37:40 +0100

    feat: prepare to run the code on SageMaker

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
+import os
+from flask import Flask, request, jsonify
 from transformers import pipeline
-import json
+
+app = Flask(__name__)
+
+# Configuration for running on SageMaker
+if 'SM_MODEL_DIR' in os.environ:
+    model_dir = os.environ['SM_MODEL_DIR']
+    config = os.path.join(model_dir, 'config.json')
+    tokenizer = os.path.join(model_dir, 'tokenizer')
+    model = os.path.join(model_dir, 'pytorch_model.bin')
+else:
+    # Default configuration for local development
+    config = 'bigscience/bloom/config.json'
+    tokenizer = 'bigscience/bloom'
+    model = 'bigscience/bloom/pytorch_model.bin'
 
 bloom_pipeline = pipeline(
     'text-generation',
-    model='bigscience/bloom',
-    tokenizer='bigscience/bloom'
+    model=model,
+    tokenizer=tokenizer,
+    config=config
 )
 
-def lambda_handler(event, context):
-    if 'body' in event:
-        body = json.loads(event['body'])
-        prompt = body['prompt']
-        output = bloom_pipeline(prompt, max_length=100)
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({"generated_text": output[0]['generated_text']})
-        }
-    else:
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({"message": "Request body not found"})
-        }
+@app.route('/generate_text', methods=['POST'])
+def generate_text():
+    prompt = request.json['prompt']
+    output = bloom_pipeline(prompt, max_length=100)
+    return jsonify({'generated_text': output[0]['generated_text']})
+
+if __name__ == '__main__':
+    app.run(debug=True)
